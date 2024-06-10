eab968f58d5da609590bce6d178cbaefab005af0
Safaa brouk <63406961+safaabrouk@users.noreply.github.com>
2024-06-10 13:53:42 +0100

    Get address by id in AddressController

--- addressMs/src/main/java/com/myapp/addressms/controllers/AddressController.java
+++ addressMs/src/main/java/com/myapp/addressms/controllers/AddressController.java
@@ -5,10 +5,12 @@ import com.myapp.addressms.services.AddressService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/address")
@@ -23,6 +25,16 @@ public class AddressController {
     @GetMapping("/addresses")
     public ResponseEntity<List<Address>> getAll() {
         return new ResponseEntity<>(addressService.getAllAddresses() , HttpStatus.OK);
+    }
+
+    // Get address by id :
+    @GetMapping("/{id}")
+    public ResponseEntity<Object>  getOne(@PathVariable int id) {
+       Optional<Address> address = addressService.getAddressById(id);
+        if(address.isPresent()) {
+            return new ResponseEntity<>(address , HttpStatus.OK);
+        }
+        return new ResponseEntity<>("Address not found !" , HttpStatus.NOT_FOUND);
     }
 
 }
