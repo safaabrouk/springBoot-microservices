4ded24867fdb16a629a1677e3b51deb5c80734e4
Safaa brouk <63406961+safaabrouk@users.noreply.github.com>
2024-06-10 15:02:20 +0100

    Add logs in controller using actuator

--- addressMs/src/main/java/com/myapp/addressms/controllers/AddressController.java
+++ addressMs/src/main/java/com/myapp/addressms/controllers/AddressController.java
@@ -3,6 +3,7 @@ package com.myapp.addressms.controllers;
 import com.myapp.addressms.entities.Address;
 import com.myapp.addressms.services.AddressService;
 import com.myapp.addressms.tools.dto.AddressDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +13,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/address")
+@Slf4j
 public class AddressController {
 
     private final AddressService addressService;
@@ -22,6 +24,7 @@ public class AddressController {
     // Get all addresses :
     @GetMapping("/addresses")
     public ResponseEntity<List<Address>> getAll() {
+        log.info("Get all addresses");
         return new ResponseEntity<>(addressService.getAllAddresses() , HttpStatus.OK);
     }
 
@@ -30,6 +33,7 @@ public class AddressController {
     public ResponseEntity<Object>  getOne(@PathVariable int id) {
        Optional<Address> address = addressService.getAddressById(id);
         if(address.isPresent()) {
+            log.info("Get address with id {}", id);
             return new ResponseEntity<>(address , HttpStatus.OK);
         }
         return new ResponseEntity<>("Address not found !" , HttpStatus.NOT_FOUND);
@@ -40,8 +44,10 @@ public class AddressController {
     public ResponseEntity<Object>  add(@RequestBody AddressDto addressDto) {
         Address address = addressService.addAddress(addressDto);
         if(address != null) {
+            log.info("Add new address {} ",address);
             return new ResponseEntity<>(address , HttpStatus.CREATED);
         }
+        log.error("Address not added");
         return new ResponseEntity<>("Address not added" , HttpStatus.BAD_REQUEST);
     }
 
@@ -51,8 +57,10 @@ public class AddressController {
     public ResponseEntity<Object>  update(@PathVariable int id, @RequestBody AddressDto addressDto) {
         Address address = addressService.updateAddress(id,addressDto);
         if(address != null) {
+            log.info("Update address with id {}", id);
             return new ResponseEntity<>(address , HttpStatus.OK);
         }
+        log.error("Address with id {} doesn't exist to update", id);
         return new ResponseEntity<>("Address not updated" , HttpStatus.NOT_FOUND);
     }
 
@@ -61,8 +69,10 @@ public class AddressController {
     public ResponseEntity<Object>  update(@PathVariable int id) {
         boolean deleted = addressService.deleteAddress(id);
         if(deleted) {
+            log.warn("Delete address with id {}", id);
             return new ResponseEntity<>("Address deleted successfully" , HttpStatus.OK);
         }
+        log.error("Address with id {} doesn't exist to delete", id);
         return new ResponseEntity<>("Address not deleted" , HttpStatus.NOT_FOUND);
     }
 
