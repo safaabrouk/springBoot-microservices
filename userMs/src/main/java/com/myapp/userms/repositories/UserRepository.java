69bb56a891307a0a550ee78954bbb37958c77b10
Safaa brouk <63406961+safaabrouk@users.noreply.github.com>
2024-06-10 15:17:19 +0100

    Add User repository

---
+++ userMs/src/main/java/com/myapp/userms/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.myapp.userms.repositories;
+
+import com.myapp.userms.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+}
