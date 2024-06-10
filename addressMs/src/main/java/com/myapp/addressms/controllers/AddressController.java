dd99d623a53c767a51eb89a23f0f3d9bf8de02f3
Safaa brouk <63406961+safaabrouk@users.noreply.github.com>
2024-06-10 13:35:09 +0100

    Add address controller

---
+++ addressMs/src/main/java/com/myapp/addressms/controllers/AddressController.java
@@ -0,0 +1,11 @@
+package com.myapp.addressms.controllers;
+
+import com.myapp.addressms.services.AddressService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/address")
+public class AddressController {
+
+}
