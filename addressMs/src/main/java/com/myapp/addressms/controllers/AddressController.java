4ce3cafb1dcd00b59606fd695f67b8ac6dc552b8
Safaa brouk <63406961+safaabrouk@users.noreply.github.com>
2024-06-10 13:37:50 +0100

    Get all addresses in AddressController

--- addressMs/src/main/java/com/myapp/addressms/controllers/AddressController.java
+++ addressMs/src/main/java/com/myapp/addressms/controllers/AddressController.java
@@ -1,8 +1,14 @@
 package com.myapp.addressms.controllers;
 
+import com.myapp.addressms.entities.Address;
 import com.myapp.addressms.services.AddressService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/address")
@@ -11,6 +17,12 @@ public class AddressController {
     private final AddressService addressService;
     public AddressController(AddressService addressService) {
         this.addressService = addressService;
+    }
+
+    // Get all addresses :
+    @GetMapping("/addresses")
+    public ResponseEntity<List<Address>> getAll() {
+        return new ResponseEntity<>(addressService.getAllAddresses() , HttpStatus.OK);
     }
 
 }
